cb064b4ce26540d21aec7c12199177772bc66464
Ashley Neaves <ashley.neaves@stfc.ac.uk>
2025-10-29 16:34:18 +0000

    cmake for the pybind11 lib now uses find_package for both hdf5 and xdma_hexitec. cmake find module for xdma_hexitec added for this purpose. Future aims are to add a .pc file to utilise pkg-config and a script to install xdma_hexitec into standard lib locations to remove the requirement for an ENV variable point to the det-software repo

---
+++ lib/cmake/Findxdma_hexitec.cmake
@@ -0,0 +1,66 @@
+# Tries to find xdma_hexitec headers and libraries
+#
+# Variables used by this module which may need to be set before calling find_package:
+#
+# DET_SOFTWARE  Set this variable to the root of William's det-software svn repo
+#
+# Variables defined by this module:
+#
+# XDMA_HEXITEC_FOUND          System has the xdma_hexitec libs/headers
+# XDMA_HEXITEC_LIBRARIES      The xdma_hexitec libraries
+# XDMA_HEXITEC_INCLUDE_DIRS   The location of the Hexitec Headers
+
+message("\nLooking for xdma_hexitec headers and libraries")
+
+if (DET_SOFTWARE)
+    message(STATUS "det-software root dir: ${DET_SOFTWARE}")
+    string(TOLOWER ${CMAKE_SYSTEM_NAME} DET_SYSTEM_NAME)
+    set(DET_SOFTWARE_INCLUDE_DIR ${DET_SOFTWARE}/libs/include)
+    set(DET_SOFTWARE_LIBS_DIR ${DET_SOFTWARE}/libs/libs.${DET_SYSTEM_NAME}.${CMAKE_HOST_SYSTEM_PROCESSOR}/lib)
+endif()
+
+
+
+find_package(PkgConfig)
+if(PkgConfig_FOUND)
+    message("using Pkgconfig")
+    pkg_check_modules(PC_XDMA_HEXITEC xdma_hexitec)
+endif(PkgConfig_FOUND)
+
+
+find_path(XDMA_HEXITEC_INCLUDE_DIRS
+    NAMES
+        xdma_hexitec.h
+    PATHS
+        ${DET_SOFTWARE_INCLUDE_DIR}
+        ${PC_XDMA_HEXITEC_INCLUDEDIR}
+        ${PC_XDMA_HEXITEC_INCLUDE_DIRS}
+)
+
+find_library(XDMA_HEXITEC_LIBRARIES
+    NAMES
+        xdma_hexitec
+    PATHS
+        ${DET_SOFTWARE_LIBS_DIR}
+        ${PC_XDMA_HEXITEC_LIBDIR}
+        ${PC_XDMA_HEXITEC_LIBRARY_DIRS}
+)
+
+# handle the QUIETLY and REQUIRED arguments and set XDMA_HEXITEC_FOUND to TRUE
+# if all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(xdma_hexitec
+    DEFAULT_MSG
+    XDMA_HEXITEC_LIBRARIES
+    XDMA_HEXITEC_INCLUDE_DIRS
+)
+
+mark_as_advanced(XDMA_HEXITEC_LIBRARIES XDMA_HEXITEC_INCLUDE_DIRS)
+
+if(XDMA_HEXITEC_FOUND)
+    message("xdma_hexitec found")
+    message(STATUS "Include directories: ${XDMA_HEXITEC_INCLUDE_DIRS}")
+    message(STATUS "Libraries: ${XDMA_HEXITEC_LIBRARIES}")
+endif()
+
